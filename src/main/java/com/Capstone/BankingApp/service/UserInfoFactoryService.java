f2ecdf2925193b595cd2fbb8d453820fa6c94430
nicosal1080 <nicosal1080@hotmail.com>
2022-05-13 13:12:47 -0500

    UserInfoFactory service and impl

---
+++ src/main/java/com/Capstone/BankingApp/service/UserInfoFactoryService.java
@@ -0,0 +1,7 @@
+package com.Capstone.BankingApp.service;
+
+import com.Capstone.BankingApp.entity.UserInfoFactory;
+
+public interface UserInfoFactoryService {
+    public UserInfoFactory showUserInfoFactoryDetails(int userId);
+}
