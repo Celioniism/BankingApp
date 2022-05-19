538217e71784362f6d0c56a72e5761614f6602ec
nicosal1080 <nicosal1080@hotmail.com>
2022-05-19 15:56:13 -0500

    AdminServiceImpl methods

--- src/main/java/com/Capstone/BankingApp/service/AdminServiceImpl.java
+++ src/main/java/com/Capstone/BankingApp/service/AdminServiceImpl.java
@@ -13,21 +13,48 @@ public class AdminServiceImpl implements AdminService{
 
     @Override
     public Administrator showAdminInfo(int adminId) {
-        return null;
+    return administratorRepo.getById(adminId);
     }
 
     @Override
     public void editAdminUsername(int adminId, Administrator administrator) {
-
+        // get adminId
+        administratorRepo.getById(adminId);
+        // admin details
+        administrator.setAdminId(administrator.getAdminId());
+        administrator.setUserPassword(administrator.getUserPassword());
+        administrator.setFullName(administrator.getFullName());
+        // update adminUsername
+        administrator.setUserName(administrator.getUserName());
+        // save updated adminUsername
+        administratorRepo.save(administrator);
     }
 
     @Override
     public void editAdminPassword(int adminId, Administrator administrator) {
-
+        // get adminId
+        administratorRepo.getById(adminId);
+        // admin details
+        administrator.setAdminId(administrator.getAdminId());
+        administrator.setUserName(administrator.getUserName());
+        administrator.setFullName(administrator.getFullName());
+        // update adminPassword
+        administrator.setUserPassword(administrator.getUserPassword());
+        // save updated adminPassword
+        administratorRepo.save(administrator);
     }
 
     @Override
     public void editAdminFullName(int adminId, Administrator administrator) {
-
+        // get adminId
+        administratorRepo.getById(adminId);
+        // admin details
+        administrator.setAdminId(administrator.getAdminId());
+        administrator.setUserName(administrator.getUserName());
+        administrator.setUserPassword(administrator.getUserPassword());
+        // update adminFullName
+        administrator.setFullName(administrator.getFullName());
+        // save updated adminFullName
+        administratorRepo.save(administrator);
     }
 }
