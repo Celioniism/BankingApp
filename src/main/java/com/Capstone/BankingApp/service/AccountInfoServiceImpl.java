508a34f21e409465062a4069cffb6ea7b17cc59e
nicosal1080 <nicosal1080@hotmail.com>
2022-05-20 15:42:16 -0500

    updated method logic

--- src/main/java/com/Capstone/BankingApp/service/AccountInfoServiceImpl.java
+++ src/main/java/com/Capstone/BankingApp/service/AccountInfoServiceImpl.java
@@ -6,6 +6,7 @@ import com.Capstone.BankingApp.repository.AccountInfoRepo;
 
 import java.util.List;
 
+import com.Capstone.BankingApp.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,42 +16,66 @@ public class AccountInfoServiceImpl implements AccountInfoService {
     @Autowired
     AccountInfoRepo accountInfoRepo;
 
+	@Autowired
+	UserRepo userRepo;
 
-    
 
 	@Override
-	public boolean approveCustomer(int accountNo) {
-		// TODO Auto-generated method stub
-		return false;
+	public void editSecurityQuestion(int accountNo, AccountInfo accountInfo) {
+		// get accountNo
+		accountInfoRepo.getById(accountNo);
+		// update securityQuestion and securityAnswer
+		accountInfo.setSecurityQuestion(accountInfo.getSecurityQuestion());
+		accountInfo.setSecurityAnswer(accountInfo.getSecurityAnswer());
+		// save updated securityQuestion
+		accountInfoRepo.save(accountInfo);
 	}
 
 	@Override
-	public List<AccountInfo> getAllAccounts(int userID, List<AccountInfo> Accountinfo) {
-		// TODO Auto-generated method stub
-		return null;
+	public void editSecurityAnswer(int accountNo, AccountInfo accountInfo) {
+		// get accountNo
+		accountInfoRepo.getById(accountNo);
+		// update securityAnswer
+		accountInfo.setSecurityAnswer(accountInfo.getSecurityAnswer());;
+		// save updated securityAnswer
+		accountInfoRepo.save(accountInfo);
 	}
 
 	@Override
-	public void editSecurityQuestion(int accountNo) {
-		// TODO Auto-generated method stub
-		
+	public boolean validateCustomer(String userName, String password) {
+		User temp = userRepo.findByUserName(userName);
+		if(temp.getUserName()==userName && temp.getPassword()==password)
+			return true;
+		else
+			return false;
 	}
 
+
 	@Override
-	public void editSecurityAnswer(int accountNo) {
-		// TODO Auto-generated method stub
-		
+	public List<AccountInfo> getAllAccounts() {
+		return accountInfoRepo.findAll();
 	}
+
 
 	@Override
 	public AccountInfo getAccountByAcctNo(int accountNo) {
-		// TODO Auto-generated method stub
-		return null;
+		return accountInfoRepo.getById(accountNo);
 	}
 
 	@Override
-	public void createAccount(int userID, AccountInfo accountinfo) {
-		// TODO Auto-generated method stub
+	public void createAccount(AccountInfo accountInfo) {
+		User user = new User();
+
+		user.setUserName(user.getUserName());
+		user.setPassword(user.getPassword());
+		user.setFullName(user.getFullName());
+
+		accountInfo.setSecurityQuestion(accountInfo.getSecurityQuestion());
+		accountInfo.setSecurityAnswer(accountInfo.getSecurityAnswer());
+		accountInfo.setCard(accountInfo.getCard());
+
+		userRepo.save(user);
+		accountInfoRepo.save(accountInfo);
 		
 	}
 }
