7093c80732a47fef72fadf04fe1cdf9782ed2744
Celioniism <79548800+Celioniism@users.noreply.github.com>
2022-05-23 12:51:18 -0400

    Update CardsService.java

--- src/main/java/com/Capstone/BankingApp/service/CardsService.java
+++ src/main/java/com/Capstone/BankingApp/service/CardsService.java
@@ -1,13 +1,18 @@
 package com.Capstone.BankingApp.service;
 
+import java.util.List;
+
 import com.Capstone.BankingApp.entity.Cards;
 
 public interface CardsService {
-    public Cards showCardDetails(int associatedId);
     
     public void registerCard(Cards card);
     
     public void deleteCard(long cardnumber);
     
     public Long generateCard();
+
+    public List<Cards> showUserCardDetails(int associatedId);
+
+	List<Cards> showAllCards();
 }
