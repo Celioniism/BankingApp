4182ce735d7fd19e34f35a3d93d86971dd449135
DChen810 <65102644+DChen810@users.noreply.github.com>
2022-05-23 12:11:46 -0500

    Add files via upload
    add cards, transaction, user, userinfofactory controllers

---
+++ src/main/java/com/Capstone/BankingApp/controller/CardsController.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package com.Capstone.BankingApp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Capstone.BankingApp.entity.Cards;
+import com.Capstone.BankingApp.service.CardsService;
+
+
+
+/**
+ * @author Dekun
+ * @Date 23 May 2022	
+ *
+ */
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/cards")
+public class CardsController {
+	@Autowired
+	CardsService cardservice;
+	
+	
+	
+	@GetMapping(value = "/showAllCards" )
+	public List<Cards> showAllCards() {
+		return cardservice.showAllCards();
+	}
+	
+	@PostMapping(value = "/registerCard")
+	public void registertCard(@RequestBody Cards card) {
+		cardservice.registerCard(card);
+	}
+	
+	@DeleteMapping(value = "/deleteCard/{cardnumber}")
+	public void deleteCard(@PathVariable("cardnumber") long cardnumber) {
+		cardservice.deleteCard(cardnumber);
+	}
+	
+	@GetMapping(value = "/generatecard")
+	public Long generateCard() {
+		return cardservice.generateCard();
+	}
+	@GetMapping({ "/showUserCardDetails/{userId}" })
+    public List<Cards> showUserCardDetails(@PathVariable("associatedId")int associatedId) {
+        List<Cards> cards = cardservice.showUserCardDetails(associatedId);
+        return cards;
+    }
+	
+	
+}
