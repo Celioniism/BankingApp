21c25575ff5bd8405f4d33a379e5a203c77754ab
DChen810 <65102644+DChen810@users.noreply.github.com>
2022-05-23 09:29:03 -0500

    Update AdministratorRepo.java

--- src/main/java/com/Capstone/BankingApp/repository/AdministratorRepo.java
+++ src/main/java/com/Capstone/BankingApp/repository/AdministratorRepo.java
@@ -3,6 +3,7 @@ package com.Capstone.BankingApp.repository;
 import com.Capstone.BankingApp.entity.Administrator;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface AdministratorRepo extends JpaRepository<Administrator, Integer> {
