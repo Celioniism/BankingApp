a3dc96124781cfaa7fc8543d3d091870b6763dce
Celioniism <79548800+Celioniism@users.noreply.github.com>
2022-05-23 14:36:37 -0400

    Update AccountInfoServiceImpl.java

--- src/main/java/com/Capstone/BankingApp/service/AccountInfoServiceImpl.java
+++ src/main/java/com/Capstone/BankingApp/service/AccountInfoServiceImpl.java
@@ -6,6 +6,7 @@ import com.Capstone.BankingApp.repository.AccountInfoRepo;
 
 import java.util.List;
 
+import com.Capstone.BankingApp.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,42 +16,64 @@ public class AccountInfoServiceImpl implements AccountInfoService {
     @Autowired
     AccountInfoRepo accountInfoRepo;
 
-
-    
+	@Autowired
+	UserRepo userRepo;
 
 	@Override
-	public boolean approveCustomer(int accountNo) {
-	
-		return false;
+	public void editSecurityQuestion(int accountNo, AccountInfo accountInfo) {
+		// get accountNo
+		accountInfoRepo.getById(accountNo);
+		// update securityQuestion and securityAnswer
+		accountInfo.setSecurityQuestion(accountInfo.getSecurityQuestion());
+		accountInfo.setSecurityAnswer(accountInfo.getSecurityAnswer());
+		// save updated securityQuestion
+		accountInfoRepo.save(accountInfo);
 	}
 
 	@Override
-	public List<AccountInfo> getAllAccounts(int userID, List<AccountInfo> Accountinfo) {
-	
-		return null;
+	public void editSecurityAnswer(int accountNo, AccountInfo accountInfo) {
+		// get accountNo
+		accountInfoRepo.getById(accountNo);
+		// update securityAnswer
+		accountInfo.setSecurityAnswer(accountInfo.getSecurityAnswer());;
+		// save updated securityAnswer
+		accountInfoRepo.save(accountInfo);
 	}
 
 	@Override
-	public void editSecurityQuestion(int accountNo) {
-	
-		
+	public boolean validateCustomer(String userName, String password) {
+		User temp = userRepo.findByUserName(userName);
+		if(temp.getUserName()==userName && temp.getPassword()==password)
+			return true;
+		else
+			return false;
 	}
 
 	@Override
-	public void editSecurityAnswer(int accountNo) {
-	
-		
+	public List<AccountInfo> getAllAccounts() {
+		return accountInfoRepo.findAll();
 	}
 
 	@Override
 	public AccountInfo getAccountByAcctNo(int accountNo) {
-	
-		return null;
+		return accountInfoRepo.getById(accountNo);
 	}
 
 	@Override
-	public void createAccount(int userID, AccountInfo accountinfo) {
-	
-		
+	public void createAccount(AccountInfo accountInfo) {
+		User user = new User();
+
+		user.setUserName(user.getUserName());
+		user.setPassword(user.getPassword());
+		user.setFullName(user.getFullName());
+
+		accountInfo.setSecurityQuestion(accountInfo.getSecurityQuestion());
+		accountInfo.setSecurityAnswer(accountInfo.getSecurityAnswer());
+		accountInfo.setCard(accountInfo.getCard());
+
+		userRepo.save(user);
+		accountInfoRepo.save(accountInfo);
 	}
+
+
 }
