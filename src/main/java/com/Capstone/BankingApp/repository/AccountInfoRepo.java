a82409a868f2ae7d95143df89280fc4420ff0354
DChen810 <65102644+DChen810@users.noreply.github.com>
2022-05-25 08:26:11 -0500

    Update AccountInfoRepo.java

--- src/main/java/com/Capstone/BankingApp/repository/AccountInfoRepo.java
+++ src/main/java/com/Capstone/BankingApp/repository/AccountInfoRepo.java
@@ -4,7 +4,7 @@ import com.Capstone.BankingApp.entity.AccountInfo;
 import com.Capstone.BankingApp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.Query;
 @Repository
 public interface AccountInfoRepo extends JpaRepository<AccountInfo, Integer> {
      @Query(value = "SELECT * FROM user WHERE user_name = ?1", nativeQuery = true)
