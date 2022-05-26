f47d071a393468a1b511834be356166f2dc24eb9
nicosal1080 <nicosal1080@hotmail.com>
2022-05-26 08:22:07 -0500

    edits
    edits

--- src/main/java/com/Capstone/BankingApp/security/controller/TestController.java
+++ src/main/java/com/Capstone/BankingApp/security/controller/TestController.java
@@ -16,19 +16,19 @@ public class TestController {
 	}
 	
 	@GetMapping("/user")
-	@PreAuthorize("hasRole('USER') or hasRole('STAFF') or hasRole('ADMIN')")
+	//@PreAuthorize("hasRole('USER') or hasRole('STAFF') or hasRole('ADMIN')")
 	public String userAccess() {
 		return "User Content.";
 	}
 
 	@GetMapping("/staff")
-	@PreAuthorize("hasRole('STAFF')")
+	//@PreAuthorize("hasRole('STAFF')")
 	public String moderatorAccess() {
 		return "Staff Board.";
 	}
 
 	@GetMapping("/admin")
-	@PreAuthorize("hasRole('ADMIN')")
+	//@PreAuthorize("hasRole('ADMIN')")
 	public String adminAccess() {
 		return "Admin Board.";
 	}
